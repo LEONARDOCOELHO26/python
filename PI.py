b76578187fb02f0b377866434eff1a4129bd51b6
LEONARDOCOELHO26 <71470229+LEONARDOCOELHO26@users.noreply.github.com>
2023-03-17 16:01:32 -0300

    Create PI.py

---
+++ PI.py
@@ -0,0 +1,21 @@
+roda_dianteira_direita = float(input("Peso da roda dianteira direita: "))
+roda_dianteira_esquerda = float(input("Peso da roda dianteira esquerda: "))
+roda_traseira_direita = float(input("Peso da roda traseira direita: "))
+roda_traseira_esquerda = float(input("Peso da roda traseira esquerda: "))
+
+distancia_dianteira_direita = float(input("distancia do centro e a ponta do eixo direito dianteiro "))
+distancia_dianteira_esquerda = float(input("distancia do centro e a ponta do eixo esquerdo dianteiro"))
+distancia_traseira_direita = float(input("distancia do centro e a ponta do eixo direito traseiro"))
+distancia_traseira_esquerda = float(input("distancia do centro e a ponta do eixo esquerdo traseiro "))
+
+distancia_apoio_dianteira_direita = float(input("distancia do apoio e a ponta do eixo direito dianteiro "))
+distancia_apoio_dianteira_esquerda = float(input("distancia do apoio e a ponta do eixo esquerdo dianteiro"))
+distancia_apoio_traseira_direita = float(input("distancia do apoio e a ponta do eixo direito traseiro"))
+distancia_apoio_traseira_esquerda = float(input("distancia do apoio e a ponta do eixo esquerdo traseiro"))
+
+
+
+print("Peso total do veículo:", pesototal)
+
+
+calculo = (pesototal)
